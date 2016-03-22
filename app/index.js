a0da8242a131e681bd47d4a6c2420005a4e75cd3
curcuz <czkzcz.cmc@gmail.com>
2016-03-22 03:17:25 +0100

    pir tests

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 (function() {
   'use strict';
-  setInterval(function alive() {
-    console.log("alive");
-  },5000);
+
+  const pin121 = require("pi-pins").connect(121);
+  pin121.mode('in');
+
+  pin121.on('rise', function () {       // …or `'fall'`, or `'both'`
+      console.log("Movement detected!");
+  });
+
 })();
