c4659d302213fd50665d3b6d3a1a99d1777c5c72
curcuz <czkzcz.cmc@gmail.com>
2016-03-22 04:30:29 +0100

    tell SAMI when movement is detected

--- README.md
+++ README.md
@@ -10,4 +10,11 @@ Resin.io application to showcase an artik-5 controlling a LED-Matrix and a PIR s
   * Set the `Retrigger Setting` jumper to __*H*__
 
   * ###### ENV config
-    * `PIR_PIN` => __*int*__ ( defaults to `121` => J27 pin 2 ) which Artik Digital PIN the PIR __*OUT*__ pin is connected 
+    * `PIR_PIN` => __*int*__ ( defaults to `121` => J27 pin 2 ) which Artik Digital PIN the PIR __*OUT*__ pin is connected
+
+* #### SAMI
+  * You need a Samsung account and a device created in the SAMI dashboard
+  * ###### ENV config
+    * `SAMI_URL` => __*string*__ ( defaults to `https://api.samsungsami.io/v1.1/messages` ) the SAMI endpoint
+    * `SAMI_DEVICE_ID` => __*string*__ the Device ID set in the SAMI dashboard
+    * `SAMI_DEVICE_TOKEN` => __*string*__ the Device token generated in the SAMI dashboard
--- app/index.js
+++ app/index.js
@@ -1,11 +1,48 @@
 (function() {
   'use strict';
 
+  const samiURL = process.env.SAMI_URL || "https://api.samsungsami.io/v1.1/messages";
+  const bearer = "Bearer "+process.env.SAMI_DEVICE_TOKEN || null;
+  const device_id = process.env.SAMI_DEVICE_ID|| null;
+  const Client = require('node-rest-client').Client;
   const pir = require("pi-pins").connect(parseInt(process.env.PIR_PIN) || 121);
+  const sami = new Client();
   pir.mode('in');
 
   pir.on('rise', function () {       // …or `'fall'`, or `'both'`
       console.log("Movement detected! PIN value is "+pir.value());
+      push2sami(pir.value(),function pushed(err,response) {
+        if (!err) {
+          console.log("Movement detection reported to SAMI");
+        } else {
+          console.error(err);
+        }
+      });
   });
+
+  function push2sami (data,cb) {
+    if (bearer && device_id) {
+      let args= {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": bearer
+            },
+        data: {
+            "sdid": device_id,
+            "ts": new Date().valueOf(),
+            "type": "message",
+            "data": {
+                "movement": data
+            }
+        }
+      };
+      sami.post(samiURL, args, function(data, response) {
+         cb(null,response);
+      });
+
+    } else {
+      cb("missing device parameter/s (id,token).");
+    }
+  }
 
 })();
