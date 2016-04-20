cb0bc8be19513d60eae7407b2278ed54504514b9
Praneeth Bodduluri <lifeeth@gmail.com>
2016-04-19 19:57:35 -0700

    tty1 settings to disable cursor + erase previous screen + turn off sleep mode

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Hide cursor
+echo -e '\033[?17;0;0c' > /dev/tty1
+# Erase screen
+echo -e '\033[2J' > /dev/tty1
+# Turn off sleep mode for screen
+echo -e '\033[9;0]' > /dev/tty1
+
 while true
 do
   node /usr/src/app/index.js
